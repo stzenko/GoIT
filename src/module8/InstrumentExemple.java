19cc5cc29888036b2a47b7a5e361ca5049d2561b
stzenko <stzenko@yandex.ru>
2016-03-31 22:36:23 +0300

    31.03 module 8

--- src/module8/InstrumentExemple.java
+++ src/module8/InstrumentExemple.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * Created by Zenovii on 31.03.2016.
+ *
  */
 public class  InstrumentExemple {
     public static void main(String[] args) {
@@ -14,20 +15,23 @@ public class  InstrumentExemple {
         musicShop.musicInstrumentList(new Piano("Piano", "Forte",75,"black","key"));
         musicShop.musicInstrumentList(new Tube("Tube","Porto",125,"gold","spiritual"));
 
-            PrintTable.Print(musicShop.musicInstrumentList);
+        PrintTable.Print(musicShop.musicInstrumentList);
 
-
-        Set<MusicInstrument> sortedFirmList = new TreeSet<MusicInstrument>((o1, o2) -> o1.firm.compareTo(o2.firm));
+        Set<MusicInstrument> sortedFirmList = new TreeSet<>((o1, o2) -> o1.firm.compareTo(o2.firm));
         sortedFirmList.addAll(musicShop.musicInstrumentList);
 
         System.out.println("\n Sorted by Firm:");
         PrintTable.Print(sortedFirmList);
 
-        Set<MusicInstrument> sortedVolume = new TreeSet<MusicInstrument>((o1, o2) -> o1.volume - o2.volume);
+        Set<MusicInstrument> sortedVolume = new TreeSet<>((o1, o2) -> o1.volume - o2.volume);
         sortedVolume.addAll(musicShop.musicInstrumentList);
         System.out.println("\n Sorted by Volume:");
         PrintTable.Print(sortedVolume);
 
     }
-}
+
+    }
+
+
+
 
