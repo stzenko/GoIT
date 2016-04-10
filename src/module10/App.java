db10e47009389d8ac780eccc5f70fcdd7f3517cc
stzenko <stzenko@yandex.ru>
2016-04-10 23:05:44 +0300

    module 10

---
+++ src/module10/App.java
@@ -0,0 +1,17 @@
+package module10;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Created by Zenovii on 10.04.2016.
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        OutputInFile outputInFile = new OutputInFile();
+        outputInFile.reader();
+       /* outputInFile.writer(); */
+    }
+
+}
--- src/module10/OutputInFile.java
+++ src/module10/OutputInFile.java
@@ -1,51 +1,70 @@
 package module10;
 
-
 import module8.*;
 import module9.ShyfrCezarya;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Created by Zenovii on 07.04.2016.
  */
-public class OutputInFile {
-    public static void main(String[] args) throws Exception {
-        MusicShop musicShop = new MusicShop();
-        musicShop.initShop();
 
-        ArrayList <MusicInstrument> musicInstrument = musicShop.getMusicInstrument();
-        String text = musicInstrument.toString();
-        String code = ShyfrCezarya.toCodeString(musicInstrument);
-        String decode = ShyfrCezarya.toDeCodeString(ShyfrCezarya.toCodeString(musicInstrument));
+public class OutputInFile {
+
+
+    private static  Object S ;
+
+    public static void reader()throws IOException {
+
+        try  {
+
+            BufferedReader reader = new BufferedReader(new FileReader("src\\module10\\norm.txt"));
+
+            String S = null;
+            while((S = reader.readLine()) != null) {
+                String s = S;
+                System.out.println(s);
+            }
+            reader.close();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writer()
+    {
+       // String text = s.toString();
+       // String code = ShyfrCezarya.toCodeString(musicInstrument);
+      //  String decode = ShyfrCezarya.toDeCodeString(ShyfrCezarya.toCodeString(musicInstrument));
 
         Writer writer = null;
-            try {
-                writer = new FileWriter("src\\module10\\module10.doc");
+        try {
+            writer = new FileWriter("src\\module10\\module10.txt");
 
-                    writer.write(String.valueOf(text+"\n"));
-                    writer.write(String.valueOf(code+"\n"));
-                    writer.write(String.valueOf(decode+"\n"));
-                    writer.write(System.getProperty("line.separator"));
+            writer.write(String.valueOf(S + "\n"));
+           //writer.write(String.valueOf(code + "\n"));
+         //   writer.write(String.valueOf(decode + "\n"));
+            writer.write(System.getProperty("line.separator"));
 
-                writer.flush();
-            } catch (Exception e) {
-                Logger.getLogger(InstrumentExemple.class.getName()).log(Level.SEVERE, null, e);
-            } finally {
-                if (writer != null) {
-                    try {
-                        writer.close();
-                    } catch (IOException ex) {
-                    }
+            writer.flush();
+        } catch (Exception e) {
+            Logger.getLogger(InstrumentExemple.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException ex) {
                 }
+            }
         }
     }
 }
+
+
 
 
