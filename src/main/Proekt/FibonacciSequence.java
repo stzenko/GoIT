7bd640ec52896e7d44e78268089b52b657d44588
stzenko <stzenko@yandex.ru>
2016-04-15 17:50:51 +0300

    project

---
+++ src/main/Proekt/FibonacciSequence.java
@@ -0,0 +1,54 @@
+package main.Proekt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) throws NegativeIntegerException, NumberFormatException{
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        String a= in.next();
+        start(a);
+
+    }
+
+    public static void start(String a) throws NegativeIntegerException, NumberFormatException {
+
+        try {
+        checkInteger(a);
+            List<Integer> result = getFibSeq(a);
+            System.out.println("Fibonacci sequence < " + a + " = " + result);
+        } catch (NumberFormatException nfe) {
+            System.out.println("[Error]: Entered data isn't integer.");
+        } catch (NegativeIntegerException nie) {
+            System.out.println("[Error]: Entered number is " + nie.getValue() + ". Number should " +
+                    "be" + " > 0.");
+        }
+    }
+
+    public static String checkInteger(String a) throws NegativeIntegerException, NumberFormatException{
+        Integer i = Integer.valueOf(String.valueOf(a));
+        if (i < 1) {
+            throw new NegativeIntegerException(i);
+        }
+        return a;
+    }
+
+    public static List<Integer> getFibSeq(String limit) {
+        Integer in = Integer.valueOf(limit);
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(1);
+        sequence.add(1);
+
+        for (int i = 2; sequence.get(i - 1) + sequence.get(i - 2) < in; i++) {
+            sequence.add(i, sequence.get(i - 1) + sequence.get(i - 2));
+        }
+        return sequence;
+
+    }
+
+}
+
+
---
+++ src/test/Proekt/FibonacciSequenceTest.java
@@ -0,0 +1,64 @@
+package test.Proekt;
+
+import main.Proekt.FibonacciSequence;
+import main.Proekt.LimitException;
+import main.Proekt.NegativeIntegerException;
+import main.module8.MusicInstrument;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zenovii on 15.04.2016.
+ */
+public class FibonacciSequenceTest {
+
+    private static FibonacciSequence fibonacciSequence;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+    fibonacciSequence= new FibonacciSequence();
+    }
+
+    @Test
+    public void testStart() throws Exception {
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testStringNumberFormat() throws Exception {
+        final String a="s";
+        fibonacciSequence.checkInteger(a);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testDoubleNumberFormat() throws Exception {
+        final String a="5.5";
+        fibonacciSequence.checkInteger(a);
+    }
+
+       @Test(expected = NegativeIntegerException.class)
+    public void testMaxLimitNumber() throws Exception {
+        final String a="-5";
+        fibonacciSequence.checkInteger(a);
+    }
+
+    @Test
+    public void testGetFibSeq() throws Exception {
+        String number = "5";
+        final List<Integer> result  = fibonacciSequence.getFibSeq(number);
+        List<Integer> s = new ArrayList<>();
+
+        s.add(1);
+        s.add(1);
+        s.add(2);
+        s.add(3);
+
+        Assert.assertEquals(s,result);
+    }
+}
